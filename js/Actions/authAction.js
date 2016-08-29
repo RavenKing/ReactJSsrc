5c7357c16179b3ad0507f342d74db0f329a19e23
Cassie1111 <495550641@QQ.COM>
2016-08-29 13:31:30 +0800

    regiter panel
    register panel

--- js/Actions/authAction.js
+++ js/Actions/authAction.js
@@ -1,13 +1,13 @@
 const AUTH_SET_TOKEN = 'AUTH_SET_TOKEN'
 const AUTH_DELETE_TOKEN = 'AUTH_DELETE_TOKEN'
 import axios from "axios"
-
+import { Modal } from 'antd';
 export function setAuthToken (parameter) {
 
 
 // http://10.97.144.117:8000/SmartOperations/services/authorization.xsodata/users?$filter=USERNAME eq 'admin'
 return dispatch=>{
-
+    console.log(parameter.username);
     axios.get("http://10.97.144.117:8000/SmartOperations/services/authorization.xsodata/AUTH?$filter=USERNAME eq '"+parameter.username+"'",{
       headers:{
         'X-My-Custom-Header':'Header-Value',
@@ -37,7 +37,7 @@ return dispatch=>{
       		}
       		else 
       		{
-			data = {
+			       data = {
 
       				authorized:false,
       				error:"password",
@@ -48,12 +48,12 @@ return dispatch=>{
       }
       else{
 
-     data = {
-    authorized:false,
-      user:null,
-      error:"username",
-      hint:"incorrect username",
-     }
+        data = {
+          authorized:false,
+          user:null,
+          error:"username",
+          hint:"incorrect username",
+        }
 
       }
 
@@ -73,4 +73,127 @@ export function invalidateAuthToken () {
     type: AUTH_DELETE_TOKEN
     
   }
+}
+//check the registering data
+export function regCheck(data){
+  return dispatch=>{
+    dispatch({type:"REG_CHECK",payload:data})
+  }
+}
+export function UserRegister(data){
+  return dispatch=>{
+    var username = data.username;
+    
+    var customer_id = data.customer_id;
+    var customer_name = data.customer_name;    
+    var role = data.role;
+    var pwd = data.pwd1;
+    var industry = data.industry;
+    var country = data.country;
+    var payload={};
+    //request configuration
+    var config = {
+      headers:{
+        'X-My-Custom-Header': 'Header-Value',
+        'content-type':'application/json'
+        },
+      auth: {
+        username: 'zengheng',
+        password: 'Sap12345'
+      }
+    };
+
+    //check user name whether exists
+    axios.get("http://10.97.144.117:8000/SmartOperations/services/authorization.xsodata/users?$filter=USERNAME eq '"+username+"'",
+      config
+      ).then(function(response){
+        if(response.data.d.results.length > 0){
+          payload = {
+            authorized:false,
+            user:null,
+            error:"reg_username",
+            hint:"username already exists"
+          }
+          dispatch({type:"REG_CHECK",payload:payload});
+         
+        }
+        else{
+          axios.get("http://10.97.144.117:8000/SmartOperations/services/authorization.xsodata/users?$orderby=USER_ID desc&top=1",
+          config).then(function(response){
+              
+              var user_id = response.data.d.results[0].USER_ID;
+              user_id = Number(user_id + 1);
+              user_id = user_id.toString();
+
+              axios.post("http://10.97.144.117:8000/SmartOperations/services/authorization.xsodata/CUST",{
+
+                      CUSTOMER_ID:customer_id,
+                      CUSTOMER_NAME:customer_name,
+                      INDUSTRY:industry,
+                      COUNTRY:country
+                    },
+                    config).then(function(response){
+
+
+                      axios.post("http://10.97.144.117:8000/SmartOperations/services/authorization.xsodata/users",{
+
+                        USER_ID:user_id,
+                        CUSTOMER_ID:customer_id,
+                        PASSWORD:pwd,
+                        USERNAME:username,
+                        ROLE:role
+                    },
+                    config).then(function(response){             
+                      payload = {
+                  
+                        error:"",
+                        hint:""
+                      }
+                      dispatch({type:"REG_CHECK",payload:payload});
+                      const modal = Modal.success({
+                        title: 'Successfully register! ',
+                        content: 'You have regitered done',
+                      });
+
+                    }).catch(function(response){
+                        console.log(response);
+                    })
+
+                      
+                    }).catch(function(response){
+                      var message = response.data.error.message.value;
+                      if(message == "Service exception: [301] unique constraint violated"){
+                        payload = {
+                          authorized:false,
+                          user:null,
+                          error:"reg_cus",
+                          hint:"customer id already exists"
+                        }
+                        dispatch({type:"REG_CHECK",payload:payload});
+                      }
+                      
+                    })
+                
+              
+    
+          }).catch(function(response){
+            console.log(response);
+          })
+
+
+
+
+        }
+       
+      }).catch(function(response){
+        console.log(response);
+      })
+
+
+
+
+
+
+    
+}
 }
