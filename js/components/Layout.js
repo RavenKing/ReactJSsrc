832268af8d15c5233b7c66cf7ab7e73f48b94946
RavenKing <I077541@PVGN50900584A.apj.global.corp.sap>
2016-08-31 15:31:38 +0800

    update set timeout

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -37,23 +37,21 @@ export default class Layout extends React.Component {
     console.log("update"+articles.refresh)
     if(articles.refresh == true || articles.fetched == true )
     {
-
       return true;
     }
-
-
   }
 
   componentWillUpdate(nextProps,nextState){
     const {auth} = this.props;
-const {user} = auth.token ; 
+ const {user} = auth.token ; 
     const {articles} = nextProps;
     if(articles.refresh ==true)
     {
+      setTimeout(function(){
 
-     this.props.dispatch(fetchArticles(user))
-
-    }
+this.props.dispatch(fetchArticles(user))
+      }.bind(this),1000)
+      }
 
   }
 
