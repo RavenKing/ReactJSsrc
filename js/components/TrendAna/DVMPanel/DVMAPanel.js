531b3cd6bc35f79e29754508569e2e01fe2e3210
RavenKing <I077541@PVGN50900584A.apj.global.corp.sap>
2016-09-23 16:38:18 +0800

    structure chagen

--- js/components/TrendAna/DVMPanel/DVMAPanel.js
+++ js/components/TrendAna/DVMPanel/DVMAPanel.js
@@ -28,20 +28,59 @@ export default class DVMAPanel extends React.Component{
 
 	render() {	
 
+
+
 		const {card} = this.props;
-		var displaydata = card.dvmanalysis.map((dvmana)=>{
+//去重
+	var data ={
+		TBL:[],
+		STRATEGY:[],
+		RET:0 
+	}
+//分类
+
+	for(var onerow of card.dvmanalysis)
+	{
+   		if(onerow.category == "TBL")
+		{
+		var liveornot	= false
+ 		data.TBL.filter((one)=>{if(one.factor_name == onerow.factor_name)liveornot= true;})
+		if(!liveornot)
+			data.TBL.push(onerow);
+		}
+		else if(onerow.category=="STA")
+		{
+		var liveornot= false
+ 		data.STRATEGY.filter((one)=>{if(one.factor_name == onerow.factor_name)liveornot= true;});
+		if(!liveornot)
+		data.STRATEGY.push(onerow)
+		}
+		else if(onerow.category=="RET")
+		data.RET=parseInt(onerow.factor_name)
+		
+	}
+	console.log(data);
+
+		var displaydata;
+		/*= card.dvmanalysis.map((dvmana)=>{
 			if(dvmana.category=="RET")
 				return  <div><h3>Retention Time:</h3><InputNumber min={12} max={999} defaultValue={parseInt(dvmana.factor_name)}></InputNumber></div>
-         	else if(dvmana.category =="TBL")
-         		return <TableForm card={dvmana}></TableForm>
-         	else if(dvmana.category == "STA")
+        */ 	
+          	/*         	else if(dvmana.category == "STA")
          		return <StrategyForm card={dvmana} />
-		});
+		});*/
+        var tableview = data.TBL.map((one)=><TableForm card={one} />);
+
+       	var strategyview = data.STRATEGY.map((one)=><StrategyForm card={one} />);
+
+       var retenview = <div><h3>Retention Time:</h3><InputNumber min={12} max={999} defaultValue={parseInt(data.RET)}></InputNumber></div>;
     	
     	return (
         
        <Card  className="strategyCard aligncenter"  title="DVM Strategy " extra={<Icon type="cross" onClick = {this.CloseCard.bind(this)} />}>
-    		{	displaydata }
+    		{	tableview }
+    		{ strategyview}
+    		{ retenview }
         </Card>
 
       );
