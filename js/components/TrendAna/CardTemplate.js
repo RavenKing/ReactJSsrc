6622ceee7740f82e6bfaf640c5f41d84b5fcd112
Cassie1111 <495550641@QQ.COM>
2016-10-20 17:26:52 +0800

    style change

--- js/components/TrendAna/CardTemplate.js
+++ js/components/TrendAna/CardTemplate.js
@@ -10,7 +10,7 @@ import { setAreaDropable } from "../../interactScript";
 export default class TemplateSelect extends React.Component {
 
    
-   componentDidMount() {
+    componentDidMount() {
 
       const props = this.props;
       this.interactable = setAreaDropable({
@@ -31,28 +31,30 @@ export default class TemplateSelect extends React.Component {
               
           }
       });
-  }
+    }
 
     ModeSelect(){
 
-      const props = this.props;
-      var that = this
-            var draggableElement = event.relatedTarget,
-              dropzoneElement = event.target;
-          var currentStatus = pageStatusDataStore.getCurrentStatus();
-          var cardId = that.props.card.id;
-          var datatype= this.props.key1;
-              var factorCate = this.props.card.category[0];
-              console.log(factorCate)
-              var factorName = this.props.card.FACTOR_NAME[0];
+        const props = this.props;
+        const { card } = props;
+        var that = this;
+        var draggableElement = event.relatedTarget,
+            dropzoneElement = event.target;
+        var currentStatus = pageStatusDataStore.getCurrentStatus();
+        var cardId = card.id;
+        
+        var datatype= this.props.key1;
+        var factorCate = card.category[0];
+        console.log(factorCate);
+        var factorName = card.FACTOR_NAME[0];
+        
+        if(datatype == "CAPA" && (factorCate == "S" || factorCate == "R")){
+            var nextStatus = "ANALYSIS_RCA_" + factorName;
 
-              if(datatype == "CAPA"){
-                var nextStatus = "ANALYSIS_RCA_" + factorName;
+            if(pageStatusDataStore.getAllStatus().indexOf(nextStatus) < 0) {
+                var sIntervalCallId;
 
-                if (pageStatusDataStore.getAllStatus().indexOf(nextStatus) < 0) {
-                  var sIntervalCallId;
-
-                  (function () {
+                (function () {
                     var addStatus = function addStatus() {
                       if (displayAreaDataStore.isStatusExisted(nextStatus) && dataPanelDataStore.isStatusExisted(nextStatus) && functionPanelDataStore.isStatusExisted(nextStatus)) {
                         clearInterval(sIntervalCallId);
@@ -83,52 +85,52 @@ export default class TemplateSelect extends React.Component {
                   pageStatusChangeActions.pageStatusChangeAction(nextStatus);
                 }
               }
-              else if (datatype == "DVM"){
+              else if (datatype == "DVM" && (factorCate == "B" || factorCate == "S")){
 
-                var nextStatus = "ANALYSIS_DVM_" + factorName;
+                  var nextStatus = "ANALYSIS_DVM_" + factorName;
 
-                if (pageStatusDataStore.getAllStatus().indexOf(nextStatus) < 0) {
-                  var sIntervalCallId;
+                  if (pageStatusDataStore.getAllStatus().indexOf(nextStatus) < 0) {
+                      var sIntervalCallId;
 
-                  (function () {
-                    var addStatus = function addStatus() {
-                      if (displayAreaDataStore.isStatusExisted(nextStatus) && dataPanelDataStore.isStatusExisted(nextStatus) && functionPanelDataStore.isStatusExisted(nextStatus)) {
-                        clearInterval(sIntervalCallId);
-                        pageStatusChangeActions.pageStatusAddAction(nextStatus);
-                      }
-                    };
+                      (function () {
+                          var addStatus = function addStatus() {
+                            if (displayAreaDataStore.isStatusExisted(nextStatus) && dataPanelDataStore.isStatusExisted(nextStatus) && functionPanelDataStore.isStatusExisted(nextStatus)) {
+                              clearInterval(sIntervalCallId);
+                              pageStatusChangeActions.pageStatusAddAction(nextStatus);
+                            }
+                          };
 
-                    var nextData = {};
+                          var nextData = {};
 
-                    nextData.style = that.props.card.style;
-                    nextData.type = "ITEM-ANA";
-                    nextData.guidArr = that.props.card.guidArr;
-                    nextData.FACTOR_NAME = that.props.card.FACTOR_NAME;
-                    nextData.category = that.props.card.category;
+                          nextData.style = that.props.card.style;
+                          nextData.type = "ITEM-ANA";
+                          nextData.guidArr = that.props.card.guidArr;
+                          nextData.FACTOR_NAME = that.props.card.FACTOR_NAME;
+                          nextData.category = that.props.card.category;
 
-                    displayAreaChangeActions.displayAreaAddPageAction(nextStatus, cardId);
-                    dataPanelItemChangeActions.dataPanelAddPageAction(nextStatus);
-                    functionPanelItemChangeActions.functionPanelAddPageAction(nextStatus);
-                    displayAreaChangeActions.displayAreaAddCardAction(nextStatus,nextData);//zengheng
+                          displayAreaChangeActions.displayAreaAddPageAction(nextStatus, cardId);
+                          dataPanelItemChangeActions.dataPanelAddPageAction(nextStatus);
+                          functionPanelItemChangeActions.functionPanelAddPageAction(nextStatus);
+                          displayAreaChangeActions.displayAreaAddCardAction(nextStatus,nextData);//zengheng
 
-                    sIntervalCallId = setInterval(function () {
-                      addStatus();
-                    }, 100);
-                    ;
-                  })();
-                } else {
-                  pageStatusChangeActions.pageStatusChangeAction(nextStatus);
-                }
+                          sIntervalCallId = setInterval(function () {
+                              addStatus();
+                          }, 100);
+                          ;
+                        })();
+                  } else {
+                    pageStatusChangeActions.pageStatusChangeAction(nextStatus);
+                  }
 
 
               }
 
 
 
-      console.log(this.props)
-      console.log(this.props.key1)
+          console.log(this.props)
+          console.log(this.props.key1)
 
-    }
+    } 
 
 
 
